4a255f297a8bee30399c38ca2d9a5e159ca02e53
Muhammadnur <148664473+m2d0v@users.noreply.github.com>
2024-05-18 16:29:09 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 "use strict";
 
+window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+	document.addEventListener("click", documentActions);
+}
+function documentActions(e) {
+	const targetElement = e.target;
+
+	if (targetElement.hasAttribute('data-goto')) {
+		const gotoElement = document.querySelector(`${targetElement.dataset.goto}`);
+		const headerHeight = document.querySelector(`.header`).offsetHeight;
+
+		if (gotoElement) {
+			window.scrollTo({
+				top: gotoElement.offsetTop - headerHeight - 20,
+				behavior: "smooth"
+			})
+		}
+		e.preventDefault();
+	}
+}
